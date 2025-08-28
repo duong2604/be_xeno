ff7a09075e1c6cab284235c30ae59d2e16ef456b
danny <duongbui@vikoisoft.com>
2025-08-28 17:27:53 +0700

    feat: calcualate  total paid per customer

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -3,15 +3,14 @@ import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-
 @Injectable()
 export class CustomersService {
   constructor(private prisma: PrismaService) {}
 
   async create(createCustomerDto: CreateCustomerDto) {
     return this.prisma.customers.create({
-      data: createCustomerDto 
-    })
+      data: createCustomerDto,
+    });
   }
 
   async findAll() {
@@ -29,5 +28,28 @@ export class CustomersService {
 
   async remove(id: number) {
     return `This action removes a #${id} customer`;
+  }
+
+  async getPaidCustomer() {
+    const results = await this.prisma.$queryRaw`
+      SELECT c.customerNumber, c.customerName, p.paymentDate
+      FROM customers c
+      LEFT JOIN payments p
+      ON c.customerNumber = p.customerNumber
+      WHERE p.paymentDate IS NOT NULL;
+     `;
+    return results;
+  }
+
+  async calcTotalPaidPerCustomer() {
+    const results = await this.prisma.$queryRaw`
+      SELECT c.customerNumber, c.customerName, SUM(p.amount) as total_paid
+      FROM payments AS p
+      JOIN customers c
+      ON c.customerNumber = p.customerNumber
+      GROUP BY c.customerNumber
+      ORDER BY total_paid DESC;
+    `;
+    return results;
   }
 }
